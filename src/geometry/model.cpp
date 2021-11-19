d77714d2dff75ea0972a074b7aaa2c87c2fb8c46
Chase Hari <chaseah2@illinois.edu>
2021-11-19 12:24:24 -0600

    Add test triangle to model constructor

--- src/geometry/model.cpp
+++ src/geometry/model.cpp
@@ -2,6 +2,22 @@
 
 Model::Model(const std::string &filepath) {
   // TO-DO: compute vertices from tinyobjloader
+
+//test with one triangle only
+  Vertex v1, v2, v3;
+  v1.position = glm::vec3(0, 1, 0);
+  v2.position = glm::vec3(1, 0, 0);
+  v3.position = glm::vec3(-1, 0, 0);
+  vertices_.push_back(v1);
+  vertices_.push_back(v2);
+  vertices_.push_back(v3);
+
+  indices_.push_back(0);
+  indices_.push_back(1);
+  indices_.push_back(2);
+
+  max_pos_ = glm::vec3{1, 1, 0};
+  min_pos_ = glm::vec3{-1, 0, 0};
 }
 
 const glm::vec3 &Model::GetMaxPosition() const { return max_pos_; }
