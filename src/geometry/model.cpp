6f90491b97011a79797c1b9a81548a378776242f
Jebbly <jeffliu604@gmail.com>
2021-12-04 00:07:54 -0600

    fix normals indexing issue

--- src/geometry/model.cpp
+++ src/geometry/model.cpp
@@ -26,9 +26,9 @@ Model::Model(const std::string &filepath) : max_pos_{std::numeric_limits<float>:
       };
 
       vertex.normal = {
-        attrib.normals[3 * index.vertex_index + 0],
-        attrib.normals[3 * index.vertex_index + 1],
-        attrib.normals[3 * index.vertex_index + 2]
+        attrib.normals[3 * index.normal_index + 0],
+        attrib.normals[3 * index.normal_index + 1],
+        attrib.normals[3 * index.normal_index + 2]
       };
 
       if (vertex.position.x < min_pos_.x) {
