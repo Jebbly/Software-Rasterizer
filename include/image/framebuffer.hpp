bdf1f29002979867674229592d26c2b612102b52
Jebbly <jeffliu604@gmail.com>
2021-12-04 00:00:59 -0600

    revert to display method and clean-up code

--- include/image/framebuffer.hpp
+++ include/image/framebuffer.hpp
@@ -7,22 +7,37 @@
 #include <stdlib.h>
 #include <iostream>
 
-constexpr int WIDTH = 100;
-constexpr int HEIGHT = 40;
+#if defined(_WIN32)
+#define NOMINMAX
+#include <Windows.h>
+const std::string ESCAPE_SEQUENCE{"\x1b["};
+#elif defined(__linux__)
+const std::string ESCAPE_SEQUENCE{"\e["};
+#endif
+
+constexpr int CHARACTER_WIDTH = 8;
+constexpr int CHARACTER_HEIGHT = 16;
+constexpr float CHARACTER_ASPECT_RATIO = ((float) CHARACTER_HEIGHT) / CHARACTER_WIDTH;
+
+constexpr int WIDTH = 3200 / CHARACTER_WIDTH;
+constexpr int HEIGHT = 1600 / CHARACTER_HEIGHT;
 
 class Framebuffer {
 public:
   Framebuffer();
 
-  friend std::ostream& operator<<(std::ostream& os, Framebuffer fb);
+  // screen buffer options
+  void Display() const;
+  void Clear();
+  void ResetScreen() const;
 
   // index from (0, 0) at bottom-left
   char &ColorAt(int x, int y);
   float &DepthAt(int x, int y);
 
 private:
-  char color_[WIDTH + 1][HEIGHT];
-  float depth_[WIDTH][HEIGHT];
+  char color_[HEIGHT][WIDTH + 1];
+  float depth_[HEIGHT][WIDTH];
 };
 
 #endif // FRAMEBUFFER_HPP
--- src/image/framebuffer.cpp
+++ src/image/framebuffer.cpp
@@ -1,39 +1,60 @@
 #include "image/framebuffer.hpp"
-#include <iostream>
 
 Framebuffer::Framebuffer() {
+  // escape sequences need to be enabled first
+#ifdef _WIN32
+  HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  DWORD dwMode = 0;
+  GetConsoleMode(hOut, &dwMode);
+  dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+  SetConsoleMode(hOut, dwMode);
+#endif
+
+  ResetScreen();
+  Clear();
+
+  // by setting additional characters
+  // the buffer can be printed all at once
   for (int y = 0; y < HEIGHT; y++) {
-    int h = HEIGHT - 1 - y;
-    for (int x = 0; x < WIDTH + 1; x++) {
-      if (x == WIDTH) {
-        color_[h][x] = '\n';
-        continue;
-      }
-      color_[h][x] = ' ';
-      depth_[h][x] = std::numeric_limits<float>::max();
-    }
+    color_[y][WIDTH] = '\n';
   }
+
   color_[HEIGHT - 1][WIDTH] = '\0';
 }
 
-std::ostream& operator<<(std::ostream& os, Framebuffer fb) {
-  os << fb.color_[0];
-  return os;
+void Framebuffer::Display() const {
+  std::cout << color_[0];
+}
+
+void Framebuffer::Clear() {
+  // clear color is a space character
+  // depth should be set to the maximum possible value
+  for (int y = 0; y < HEIGHT; y++) {
+    for (int x = 0; x < WIDTH; x++) {
+      color_[y][x] = ' ';
+      depth_[y][x] = std::numeric_limits<float>::max();
+    }
+  }
+
+  std::cout << ESCAPE_SEQUENCE + "0;0H";
+}
+
+void Framebuffer::ResetScreen() const {
+  std::cout << ESCAPE_SEQUENCE + "2J";
 }
 
 char &Framebuffer::ColorAt(int x, int y) {
-  if (x < 0 || x >= WIDTH * HEIGHT) {
+  if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
     throw std::runtime_error("Framebuffer color index out of bounds!");
   }
-  int h = HEIGHT - 1 - y;
-  return color_[h][x];
+
+  return color_[HEIGHT - 1 - y][x];
 }
 
 float &Framebuffer::DepthAt(int x, int y) {
   if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
     throw std::runtime_error("Framebuffer depth index out of bounds");
   }
-  int h = HEIGHT - 1 - y;
 
-  return depth_[h][x];
+  return depth_[HEIGHT - 1 - y][x];
 }
